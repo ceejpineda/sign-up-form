6e144579e8bb82547d561e10512c789e60d25cb2
CJ <pinedacj1020@gmail.com>
2022-11-16 02:13:03 +0800

    all styling almost done, only validation left

--- js/script.js
+++ js/script.js
@@ -30,3 +30,56 @@ const angle = (cx,cy,ex,ey) => {
 };
 
 eyeEffect();
+
+const login = document.querySelectorAll(".login-input");
+const cat = document.getElementById("char1");
+const eye1 = document.getElementById("eyeLeft");
+const eye2 = document.getElementById("eyeRight");
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    login.forEach(element => {
+        if(element.value != "" ){
+            element.parentElement.classList.add("animation");
+            element.parentElement.classList.add("animation-color");
+        }
+    });
+})
+
+login.forEach(element => {
+    element.addEventListener('focus', ()=>{
+        element.parentElement.classList.add("animation");
+        element.parentElement.classList.add("animation-color");
+    })
+    element.addEventListener('focusout', ()=>{
+        if(element.value != '') return;
+        element.parentElement.classList.remove("animation");
+        element.parentElement.classList.remove("animation-color");
+    })
+});
+document.addEventListener('keypress', doThis);
+document.addEventListener('click', doThis)
+
+function doThis() {
+    let count = 0;
+    login.forEach(element => {
+        if(element.value != ''){
+            count++;
+        }
+    });
+    if(count < 2){
+        cat.src = "./images/cat1.png";
+        eye2.style.visibility = "visible";
+        eye1.style.visibility = "visible";
+    }
+    if(count>=2){
+        cat.src = "./images/cat2.png";
+        eye2.style.visibility = "hidden";
+    }if(count >= 4){
+        cat.src = "./images/cat3.png";
+        eye2.style.visibility = "visible";
+    }if(count >= 7){
+        cat.src = "./images/cat4.png";
+        eye2.style.visibility = "hidden";
+        eye1.style.visibility = "hidden";
+    }
+}
